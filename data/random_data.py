7496b9ec91eb5fa90ea606a6461c0c7be2c120fc
Zahid <i200469@nu.edu.pk>
2023-12-09 23:28:28 +0500

    Update sensor data file and file path in random_data.py

--- data/random_data.py
+++ data/random_data.py
@@ -65,6 +65,6 @@ if __name__ == "__main__":
     # dummy_data = generate_dummy_data(start_date, end_date, num_machines=5, num_sensors=3)
 
     # # Save dummy data to CSV file
-    # dummy_data.to_csv('dummy_sensor_data.csv', index=False)
+    # dummy_data.to_csv(data_file_path, index=False)
 
     generate_and_append_data(data_file_path)
