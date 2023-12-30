8086ca4442ac0bda3678f312aeade39719c89d08
Zahid <i200469@nu.edu.pk>
2023-12-30 23:02:21 +0500

    Update model loading in monitor.py

--- train/monitor.py
+++ train/monitor.py
@@ -34,7 +34,9 @@ def monitor(data_path):
         return accuracy
     
     # Load model
-    model=load_model("rf-model")
+    model_uri = mlflow.get_artifact_uri("rf-model")
+    model = mlflow.sklearn.load_model(model_uri)
+
     # Log metrics
     mse = evaluate(model, X_test, y_test)
     if mse > 72.0:
